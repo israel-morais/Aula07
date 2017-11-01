542d96b0ce861da05f6f2d434248dd459f40839c
Israel Morais Magalhães <israel-morais@live.com>
2017-11-01 15:54:17 -0200

    Commit inicial Curso em Vídeo

---
+++ Aula07/src/Lutador.java
@@ -0,0 +1,93 @@
+
+public class Lutador {
+
+	private String nome;
+	private String nacionalidade;
+	private int idade;
+	private double altura;
+	private double peso;
+	private String categoria;
+	private int vitorias;
+	private int derrotas;
+	private int empates;
+	
+	public Lutador() {
+		
+	}
+
+	private String getNome() {
+		return nome;
+	}
+
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	private String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	private void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	private int getIdade() {
+		return idade;
+	}
+
+	private void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	private double getAltura() {
+		return altura;
+	}
+
+	private void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	private double getPeso() {
+		return peso;
+	}
+
+	private void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	private String getCategoria() {
+		return categoria;
+	}
+
+	private void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	private int getVitorias() {
+		return vitorias;
+	}
+
+	private void setVitorias(int vitorias) {
+		this.vitorias = vitorias;
+	}
+
+	private int getDerrotas() {
+		return derrotas;
+	}
+
+	private void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+
+	private int getEmpates() {
+		return empates;
+	}
+
+	private void setEmpates(int empates) {
+		this.empates = empates;
+	}
+	
+	
+
+	
+}
