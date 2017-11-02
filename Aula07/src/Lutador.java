3d4d6f8b8162f605d25800bc2b7d5404e43e6406
Israel Morais Magalhães <israel-morais@live.com>
2017-11-02 21:43:13 -0200

    criação do objeto

--- Aula07/src/Lutador.java
+++ Aula07/src/Lutador.java
@@ -4,89 +4,133 @@ public class Lutador {
 	private String nome;
 	private String nacionalidade;
 	private int idade;
-	private double altura;
-	private double peso;
+	private float altura;
+	private float peso;
 	private String categoria;
-	private int vitorias;
-	private int derrotas;
-	private int empates;
+	private int vitorias, derrotas, empates;
 	
-	public Lutador() {
-		
+	// Métodos públicos
+	public void apresentar() {
+		System.out.println("------------------------------------------------------");
+		System.out.println("CHEGOU A HORA! Apresentamos o lutador " + this.getNome());
+		System.out.println("Diretamente de " + this.getNacionalidade());
+		System.out.println("com " + this.getIdade() + " anos e " + this.getAltura() + " de altura");
+		System.out.println("pesando " + this.getPeso() + " Kg");
+		System.out.println(this.getVitorias() + " Vitórias");
+		System.out.println(this.getDerrotas() + " derrotas e");
+		System.out.println(this.getEmpates() + " empates!");
+	}
+	public void status() {
+		System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+		System.out.println("Ganhou " + this.getVitorias() + " vezes");
+		System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+		System.out.println("Empatou " + this.getEmpates() + " vezes");
+	}
+	public void ganharLuta() {
+		this.setVitorias(this.getVitorias() + 1);
+	}
+	public void perderLuta() {
+		this.setDerrotas(this.getDerrotas() + 1);
+	}
+	public void empatarLuta() {
+		this.setEmpates(this.getEmpates() + 1);
+	}
+	
+	// Métodos especiais
+	
+	public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+		this.nome = no;
+		this.nacionalidade = na;
+		this.idade = id;
+		this.altura = al;
+		this.setPeso(pe);
+		this.vitorias = vi;
+		this.derrotas = de;
+		this.empates = em;
 	}
 
-	private String getNome() {
+	public String getNome() {
 		return nome;
 	}
 
-	private void setNome(String nome) {
-		this.nome = nome;
+	public void setNome(String no) {
+		this.nome = no;
 	}
 
-	private String getNacionalidade() {
+	public String getNacionalidade() {
 		return nacionalidade;
 	}
 
-	private void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
+	public void setNacionalidade(String na) {
+		this.nacionalidade = na;
 	}
 
-	private int getIdade() {
+	public int getIdade() {
 		return idade;
 	}
 
-	private void setIdade(int idade) {
-		this.idade = idade;
+	public void setIdade(int id) {
+		this.idade = id;
 	}
 
-	private double getAltura() {
+	public double getAltura() {
 		return altura;
 	}
 
-	private void setAltura(double altura) {
-		this.altura = altura;
+	public void setAltura(float al) {
+		this.altura = al;
 	}
 
-	private double getPeso() {
+	public double getPeso() {
 		return peso;
 	}
 
-	private void setPeso(double peso) {
-		this.peso = peso;
+	public void setPeso(float pe) {
+		this.peso = pe;
+		this.setCategoria();
 	}
-
+	
 	private String getCategoria() {
 		return categoria;
 	}
-
-	private void setCategoria(String categoria) {
-		this.categoria = categoria;
+	
+	private void setCategoria() {
+		if (this.peso < 52.2) {
+			this.categoria = "Inválido";
+		} else if (this.peso <= 70.3) {
+			this.categoria = "Leve";
+		} else if (this.peso <= 83.9) {
+			this.categoria = "Médio";
+		} else if (this.peso <= 120.2) {
+			this.categoria = "Pesado";
+		} else {
+			this.categoria = "Inválido";
+		}
 	}
 
-	private int getVitorias() {
+	public int getVitorias() {
 		return vitorias;
 	}
 
-	private void setVitorias(int vitorias) {
-		this.vitorias = vitorias;
+	public void setVitorias(int vi) {
+		this.vitorias = vi;
 	}
 
-	private int getDerrotas() {
+	public int getDerrotas() {
 		return derrotas;
 	}
 
-	private void setDerrotas(int derrotas) {
-		this.derrotas = derrotas;
+	public void setDerrotas(int de) {
+		this.derrotas = de;
 	}
 
-	private int getEmpates() {
+	public int getEmpates() {
 		return empates;
 	}
 
-	private void setEmpates(int empates) {
-		this.empates = empates;
+	public void setEmpates(int em) {
+		this.empates = em;
 	}
-	
-	//Alteração -------------
+
 	
 }
